62cfc03cfc4161b9f0d971312d72240f70389847
Edward Ho <pythoned77@gmail.com>
2022-06-09 16:53:23 +0800

    migrate current models success

--- db/migrate/20220609033851_create_facilities.rb
+++ db/migrate/20220609033851_create_facilities.rb
@@ -5,7 +5,6 @@ class CreateFacilities < ActiveRecord::Migration[6.1]
       t.string :address
       t.integer :phone
       t.string :price_range
-      t.string :verified_status
       t.boolean :verified_status
       t.date :verified_date
       t.references :category, null: false, foreign_key: true
