555d2ca2d57a5931411156673f836b6008edbd24
Edward Ho <pythoned77@gmail.com>
2022-06-10 22:33:04 +0800

    added review form in facilities page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,10 +4,23 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @facility = Facility.find(params[:facility_id])
+    @review = Review.new(review_params)
+    @review.facility = @facility
+    if @review.save
+      redirect_to facility_path(@facility)
+    else
+      render "facilities/show"
+    end
 
   end
 
   def show
 
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
 end
