6f1e2cac2e4e87b094c1172a7c76dcd9aa449703
Edward Ho <pythoned77@gmail.com>
2022-06-12 09:42:15 +0800

    able to show both review and message in reviewChannel

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,5 +2,7 @@ class ChannelsController < ApplicationController
   def community
     @channel = Channel.where(channel_id: 1)
     @messages = Message.all
+    @users = User.all
+    @reviews = Review.all
   end
 end
