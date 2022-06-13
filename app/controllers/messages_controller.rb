e71d42bd0b7c78eb436f08b854cdfed66d12671b
Edward Ho <pythoned77@gmail.com>
2022-06-13 23:19:03 +0800

    managed to add message in community channel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,21 @@
 class MessagesController < ApplicationController
-  def create
+  skip_before_action :verify_authenticity_token, only: [ :create ]
 
+  def create
+    @channel = Channel.find(params[:channel_id])
+    @message = Message.new(message_params)
+    @message.channel = @channel
+    @message.user = current_user
+    if @message.save
+      redirect_to community_path(anchor: "message-#{@message.id}")
+    else
+      render "channels/show"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
