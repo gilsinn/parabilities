91169a0e7db090fc2e4931b3dbcde04163ba4eb9
Edward Ho <pythoned77@gmail.com>
2022-06-16 06:30:26 +0800

    attempt to add display form for comment section - javascript not working

---
+++ app/javascript/controllers/display_comment_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["comment", "form"];
+
+  connect() {
+    console.log(displaying)
+  }
+
+  displayForm() {
+    this.commentTarget.classList.add("d-none")
+    this.formTarget.classList.remove("d-none")
+  }
+}
