15ea72ff84797c15d1216c324638f346c6373df1
Huiling Tan <huilingt@hotmail.com>
2022-06-15 11:14:00 +0800

    change Review Now path on facility show page to login page instead of sign up page if user not login

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,5 +7,5 @@ class Review < ApplicationRecord
   has_one_attached :photo
 
   validates :content, :user_id, :facility_id, presence: true
-  validates :photo, attached: true
+  # validates :photo, attached: true
 end
