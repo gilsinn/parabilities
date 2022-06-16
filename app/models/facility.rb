82391187d4c6e67263fc2e88135224289bbcc9f8
Aishath Muzna Muneez <aishathmmuneez@gmail.com>
2022-06-16 13:26:49 +0800

    map geocode

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -7,7 +7,8 @@ class Facility < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
   has_one_attached :photo
-
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
 
   include PgSearch::Model
